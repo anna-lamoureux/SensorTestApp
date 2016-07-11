c206ee60132ffa8417c5925bd9dbecb002d29dfe
Anna Lamoureux <annalamoureux@brns-mbp-2.students.moravian.edu>
2016-07-11 14:57:45 -0400

    ading files

---
+++ main.py
@@ -0,0 +1,30 @@
+import kivy
+kivy.require('1.8.0')
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+
+class LoginScreen(Screen):
+    pass
+
+class InstructionScreen(Screen):
+    pass
+
+class ProgressScreen(Screen):
+    pass
+
+class ResultsScreen(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+app = Builder.load_file("screens.kv")
+
+class ScreensApp(App):
+    def build(self):
+        return app
+
+ScreensApp().run()
