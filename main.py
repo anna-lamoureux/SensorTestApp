1bd5b85f9bd2ab3bad64e08ce45da7e39f9de81d
zambeezy <stnvz01@moravian.edu>
2016-07-13 10:55:39 -0400

    Adds working progressbar to basic structure.

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.progressbar import ProgressBar
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
 
 class LoginScreen(Screen):
     pass
@@ -20,7 +24,31 @@ class ResultsScreen(Screen):
     pass
 
 class ProgressBarScreen(Screen):
-		pass
+
+    progress_bar = ObjectProperty()
+
+    def __init__(self, **kwa):
+        super(ProgressBarScreen, self).__init__(**kwa)
+
+        self.progress_bar = ProgressBar()
+        self.popup = Popup(
+            title='Download',
+            content=self.progress_bar
+        )
+        self.popup.bind(on_open=self.puopen)
+        self.add_widget(Button(text='Download', on_release=self.pop))
+
+    def pop(self, instance):
+        self.progress_bar.value = 1
+        self.popup.open()
+
+    def next(self, dt):
+        if self.progress_bar.value>=100:
+            return False
+        self.progress_bar.value += 1
+
+    def puopen(self, instance):
+        Clock.schedule_interval(self.next, 1/25)
 
 class ScreenManagement(ScreenManager):
     pass
