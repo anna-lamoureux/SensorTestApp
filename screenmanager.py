26b305c7231c1590a06d3738700ccbe5e907359c
Anna Lamoureux <annalamoureux@brns-mbp-2.students.moravian.edu>
2016-07-18 11:05:10 -0400

    Creates a new user window that lets user input user name. Currently only prints new username but should save it

--- screenmanager.py
+++ screenmanager.py
@@ -4,8 +4,9 @@ kivy.require('1.8.0')
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.dropdown import DropDown
@@ -13,15 +14,18 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
+from kivy.uix.textinput import TextInput
 
 class LoginScreen(Screen):
     pass
 
 class TestScreen(Screen):
-    pass
+    def saveTest(self, testname):
+        print('Selected Test: {test}'.format(test=testname))
 
 class UserScreen(Screen):
-    pass
+    def saveUser(self, username):
+        print('Selected User: {user}'.format(user=username))
 
 class ProgressBarScreen(Screen):
     progress_bar = ObjectProperty()
@@ -31,11 +35,13 @@ class ProgressBarScreen(Screen):
 
         self.progress_bar = ProgressBar()
         self.popup = Popup(
-            title='Download',
+            title='Instructions',
             content=self.progress_bar
         )
         self.popup.bind(on_open=self.puopen)
-        self.add_widget(Button(text='Download', on_release=self.pop))
+        self.add_widget(Label(text='Here are the directions as to how to set up the test equipment.',
+                              size_hint=[.8, .9], pos_hint_x={'x':.1}))
+        self.add_widget(Button(text='Run Test', on_release=self.pop, size_hint=[.4, .05], pos_hint={'x':.3, 'y':.1}))
 
     def pop(self, instance):
         self.progress_bar.value = 1
@@ -48,6 +54,12 @@ class ProgressBarScreen(Screen):
 
     def puopen(self, instance):
         Clock.schedule_interval(self.next, 1/25)
+
+class CreateNewUserScreen(Screen):
+
+    def createNewUser(self, string):
+        print('Newly Created User: {}'.format(string))
+
 
 class ScreenManagement(ScreenManager):
     pass
